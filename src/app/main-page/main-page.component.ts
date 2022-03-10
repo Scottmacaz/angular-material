6467a54b551f55e11de32c16fedf93df0e4b869e
Scott Macmillan <smacmillan@its4logistics.com>
2022-03-10 08:57:18 -0800

    Created flex box for snack bar alignment changed from button to link for dismiss.

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -45,7 +45,7 @@ export class MainPageComponent implements OnInit {
         this.snackBar.openFromComponent(ItsSnackBar2Component, {
           data: {
             snackBarRef,
-            message: "This is a test 123 ....."
+            message: "This is a test 123 blah  blah blah blah blah blah blah blah....."
           },
           duration: 300000000,
           horizontalPosition: 'center',
